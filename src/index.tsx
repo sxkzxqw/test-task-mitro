1f56e251d3896372046bb0877fdda52a1fb60c2e
sxkzxqw <marsel.vagizov@bk.ru>
2023-05-22 20:15:02 +0300

    comments in index.tsx to reviewer

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,14 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// Добрый день!
+// Хотел предупредить что в силу некоторых противоречий ТЗ
+// и небольшого несоответсивия с ним jsonplaceholder некоторые фичи получились не очень чистыми по коду,
+// например для того чтобы отображать пост с пользователем нужно одновременно запрашивать
+// и пользователя, и сам пост, и тому подобное, поэтому прошу заранее простить :)
+// Был бы рад фидбэку по данному заданию!
+
 root.render(
   <Provider store={store}>
     <BrowserRouter basename='/test-task-mitro'>
